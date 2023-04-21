71f06085be1aae5357438d0508641fbb360fb172
Aidan-boop <70720551+Aidan-boop@users.noreply.github.com>
2023-04-21 15:45:20 -0400

    Create config.fish

---
+++ dot/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+ # Commands to run in interactive sessions can go here
+end
+function fish_greeting
+
+end
+
+colorscript random
+
+### ALIASES ###
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+
+fish_add_path /home/aidan/.spicetify
